ac709f98b76d806b536bf279c36fa6b2115e9795
ClassyDragon <cgandeg@gmail.com>
2019-10-31 09:13:23 -0400

    modified:   bitmap_read 	modified:   source.cpp

--- source/source.cpp
+++ source/source.cpp
@@ -1,26 +1,58 @@
 
 #include <iostream>
 
+static bool debug_mode = true;
+
+void Debug_Log(std::string message) {
+    if (debug_mode) {
+        std::cout << message << std::endl;
+    }
+}
+
+void read_bitmap(const char* filename, unsigned char info[]) {
+    FILE* pfile = fopen(filename, "rb"); // Open bitmap in read binary mode.
+    if (pfile == NULL) { // Exit program if file is not found.
+        std::cout << "Could not open file: " << filename << std::endl;
+    }
+    // Check that file is a bitmap:
+    std::string s(filename);
+    if (s.size() < 4) {
+        std::cout << "File is not a bitmap image." << std::endl;
+        exit(1);
+    }
+    if (s.substr(s.size() - 4, 4) != ".bmp") {
+        std::cout << "File is not a bitmap image." << std::endl;
+        exit(1);
+    }
+
+    /*
+     * fread usage: fread(void* ptr, size_t size, size_t count, FILE* file)
+     * void* ptr: pointer to block of memory to write to, converted to void* type.
+     * size_t size: Size of each element.
+     * sizez_t count: number of elements.
+     * FILE* file: File stream to read from.
+     */
+    fread(info, sizeof(unsigned char), 54, pfile);
+
+    fclose(pfile);
+}
+
 void display_data(const char* filename) {
 }
 
 int main(int argc, char** argv) {
-    FILE* pfile;
+    unsigned char info[54];
     if (argc >= 2) {
-        pfile = fopen(argv[1], "rb");
-        if (pfile == NULL) {
-            std::cout << "An error occurred..." << std::endl;
-            return 1;
-        }
+        read_bitmap(argv[1], info);
     }
     else {
         return 1;
     }
 
-    unsigned char info[54];
-
-    fread(info, sizeof(unsigned char), 54, pfile);
-    
+    /*
+     * The width is a 4-byte value stored at offset 18.
+     * The height is a 4-byte value stored at offset 22.
+     */
     int width = *((int*)(&info[18]));
     int height = *((int*)(&info[22]));
     std::cout << "Width: " << width << std::endl;
@@ -31,9 +63,5 @@ int main(int argc, char** argv) {
     //unsigned char* pixel_data = new unsigned char[size_of_array];
 
     //fread(pixel_data, sizeof(unsigned char), size_of_array, pfile);
-    fclose(pfile);
-
-
-
     return 0;
 }
